6dbed1bd0cce08ef95c5b9f25529dfd48b9d403d
3rtert <ernestdzi@interia.pl>
2023-03-04 16:15:42 +0100

    fixes to create password view

--- src/com/passm/view/CreatePasswordView.java
+++ src/com/passm/view/CreatePasswordView.java
@@ -9,7 +9,10 @@ public class CreatePasswordView extends ConsoleView<CreatePasswordController, Cr
 	
 	private final static String LOGIN_MESSAGE = "Login: ";
 	private final static String PASSOWRD_MESSAGE = "Password: ";
+	private final static String PASSWORD_CONFIRMATION_MESSAGE = "Password (again): ";
 	private final static String NAME_MESSAGE = "Name (webside/program): ";
+	private final static String EMPTY_FIELD_WARNING = "This field cannot be empty";
+	private final static String DIFFERENT_PASSWORDS_WARNING = "Passwords must be the same";
 	
 	public CreatePasswordView(Console console) {
 		super(console);
@@ -26,26 +29,60 @@ public class CreatePasswordView extends ConsoleView<CreatePasswordController, Cr
 		super.update();
 		Console console = getConsole();
 		console.ln(false);
-		String login = null, password = null, name = null;
+		String login = receiveLogin(console);
+		String password = receivePassword(console);
+		String name = receiveName(console);
+		controller.createNewPassword(login, password, name);
+	}
+	
+	private String receiveLogin(Console console) {
+		String login = null;
 		boolean isLoginCorrect = false;
 		while(!isLoginCorrect) {
 			console.print(LOGIN_MESSAGE);
 			login = console.readLine();
 			isLoginCorrect = login.length() > 0;
+			if(!isLoginCorrect) {
+				console.println(EMPTY_FIELD_WARNING);
+			}
 		}
+		return login;
+	}
+	
+	private String receivePassword(Console console) {
+		String password = null, passwordConfirmation = null;
 		boolean isPasswordCorrect = false;
 		while(!isPasswordCorrect) {
 			console.print(PASSOWRD_MESSAGE);
-			password = console.readLine();
-			isPasswordCorrect = password.length() > 0;
+			password = console.readLine(true);
+			console.ln();
+			if(password.length() > 0) {
+				console.print(PASSWORD_CONFIRMATION_MESSAGE);
+				passwordConfirmation = console.readLine(true);
+				console.ln();
+				isPasswordCorrect = password.equals(passwordConfirmation);
+				if(!isPasswordCorrect) {
+					console.println(DIFFERENT_PASSWORDS_WARNING);
+				}
+			} else {
+				console.println(EMPTY_FIELD_WARNING);
+			}
 		}
+		return password;
+	}
+	
+	private String receiveName(Console console) {
+		String name = null;
 		boolean isNameCorrect = false;
 		while(!isNameCorrect) {
 			console.print(NAME_MESSAGE);
 			name = console.readLine();
 			isNameCorrect = name.length() > 0;
+			if(!isNameCorrect) {
+				console.println(EMPTY_FIELD_WARNING);
+			}
 		}
-		controller.createNewPassword(login, password, name);
+		return name;
 	}
 	
 	@Override
